2e07bf0c86be46d0fe3c5296407bf726049c75bb
no-kris <kris.tresk@gmail.com>
2025-09-17 17:53:22 -0400

    Update: Added component to view prayers list and also added some demo prayers.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-// import PrayerList from "./components/PrayerList";
-// import AddPrayerForm from "./components/AddPrayerForm";
-import "./styles.css";
+import PrayerList from "./components/PrayerList";
 import AddPrayerForm from "./components/AddPrayerForm";
+import "./styles.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [showForm, setShowForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [prayersList, setPrayersList] = useState([
+    {
+      id: 1,
+      text: "Thank You Lord for a new day and Your blessings.",
+      category: "Thanksgiving",
+    },
+    {
+      id: 2,
+      text: "Please bring healing to my friend who is sick.",
+      category: "Healing",
+    },
+    {
+      id: 3,
+      text: "Guide me in my work decisions this week.",
+      category: "Requests",
+    },
+    {
+      id: 4,
+      text: "Help me grow in faith and trust in You.",
+      category: "General",
+    },
+  ]);
   const categories = [
     { name: "General", color: "#3b82f6" },
     { name: "Thanksgiving", color: "#eab308" },
@@ -29,11 +51,23 @@ function App() {
         ) : null}
         <main className="main">
           <Sidebar categories={categories} />{" "}
-          <section>{/* <PrayerList /> */}</section>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <PrayerList
+              prayersList={prayersList}
+              setPrayersList={setPrayersList}
+              categories={categories}
+            />
+          )}
         </main>
       </div>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">loading...</p>;
 }
 
 export default App;
---
+++ src/components/PrayerList.js
@@ -0,0 +1,33 @@
+function PrayerList({ prayersList, setPrayersList, categories }) {
+  if (prayersList.length === 0) {
+    return (
+      <p className="message">
+        You have no prayers. Try changing categories or add a new prayer.
+      </p>
+    );
+  }
+  return (
+    <section>
+      <p className="message">Your Prayers</p>
+      <ul>
+        {prayersList.map((prayer) => (
+          <li className="prayer">
+            <p>{prayer.text}</p>
+            <span
+              className="tag"
+              style={{
+                backgroundColor: categories.find(
+                  (cat) => cat.name === prayer.category
+                )?.color,
+              }}
+            >
+              {prayer.category}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default PrayerList;
